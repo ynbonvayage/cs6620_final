55c547b1da922d71ec92fad6eda444e533022af6
yinna <yinnalucky@gmail.com>
2026-06-15 18:58:06 -0700

    demo: test file with vulnerabilities

---
+++ demo-test.js
@@ -0,0 +1,6 @@
+const apiKey = "ABCDEFGHIJKLMNOP1234";
+const password = "admin123456";
+const db = mongoose.connect(req.body.url);
+db.find(req.query);
+eval(userInput);
+res.innerHTML = req.body.data;
